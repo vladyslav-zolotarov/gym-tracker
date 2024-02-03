e7af7f67a733e26bc4cc0d4541dafb8cf6551a4e
Vladyslav <vladyslav.zolotarov@gmail.com>
2024-02-03 02:03:07 +0100

    refactor: clear project

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import * as path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@components': path.resolve(__dirname, './src/components'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.REACT_APP_PB_LINK': JSON.stringify(env.REACT_APP_PB_LINK),
     },
-  },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@assets': path.resolve(__dirname, './src/assets'),
+        '@components': path.resolve(__dirname, './src/components'),
+      },
+    },
+  };
 });
